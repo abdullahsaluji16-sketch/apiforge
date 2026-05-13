28baf10ccab143b80414deef53fbc1d11e8a0126
abdullahsaluji16-sketch <abdullahsaluji16@gmail.com>
2026-05-13 22:53:42 +0530

    Update google_oauth.php

--- application/config/google_oauth.php
+++ application/config/google_oauth.php
@@ -3,5 +3,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['google_client_id']     = '397973983233-oufttnmhav8mcb6611nb536dll44fkf7.apps.googleusercontent.com';
 $config['google_client_secret'] = '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';
-$config['google_redirect_uri']  = 'http://apiforge.infinityfree.me/auth/google_callback';
+$config['google_redirect_uri']  = 'https://apiforge-39pn.onrender.com/auth/google_callback';
 $config['google_scopes']        = 'email profile';
